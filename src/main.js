2ab9c17bef04c144335162dd69ff1186444b562f
Bac Truong Van <bactruongvan17@gmail.com>
2024-05-18 16:38:23 +0700

    add cors

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ dotenv.config();
 
 import express from "express";
 import openWeatherMapRouter from './openweathermap/route.js';
+import cors from "cors";
+
+const corsOptions = {
+  origin: process.env.ALLOW_DOMAIN,
+  optionsSuccessStatus: 200
+}
 
 const app = express();
 
 const port = process.env.APP_PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', cors(corsOptions), (req, res) => {
+  res.send('Server API belongs to <a href="https://github.com/bactruongvan17">https://github.com/bactruongvan17</a>')
 });
 
-app.use('/openweathermap', openWeatherMapRouter);
+app.use('/openweathermap', cors(corsOptions), openWeatherMapRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
